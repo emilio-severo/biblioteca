5adfb5b85d62b5c5c67895f56adcda97b9f2c8b1
emilio-severo <emilio.severo@gmail.com>
2020-09-27 17:28:16 -0300

    inseri um comentario no index

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const conexao = require('./bd/conexao');
 const Livros = require('./bd/Livros');
 const bodyParser = require('body-parser');
 
+//Instancia o aplicativo do Express.
 const app = express();
 
 app.use(express.static("public"));
